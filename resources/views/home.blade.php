248b05014e494693eafa4e631da5723b3adac2a8
giadavolpin <volpingiada@gmail.com>
2022-12-18 17:02:17 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content') {{-- struttura main uguale da per tutto --}}
+    <h1>home page</h1>
+
+@endsection
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,60 @@
+<header class="bg-white">
+    <div id="logo" class="text-center">
+        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo">
+    </div>
+    <nav class="navbar-nav container navbar-light">
+        <ul class="list-unstyled d-flex justify-content-center gap-2 text-uppercase">
+            <li class="nav-item">
+                <a>
+                  characters  
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                  comics
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    movies                    
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    tv
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    games
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    collectibles
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    videos
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                    fans
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a>
+                   news 
+                </a>
+            </li>
+            <li class="nav-item">
+                <a>
+                   shop 
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
