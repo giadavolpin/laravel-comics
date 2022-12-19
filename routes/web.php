7ac54cbc1e33de33684b105047fc929e465ab00e
giadavolpin <volpingiada@gmail.com>
2022-12-19 16:11:07 +0100

    prova

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
+/* 
+Route::get('/', function(){
+return view('fumetti')
+}
+) */
 
 
 // Route::get('/percorsoURL', function () {
